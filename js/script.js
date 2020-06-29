e8a044bd751bf90b2db3efaff493abc0c2aadbc0
Max <www.lysenko@gmail.com>
2020-06-29 12:33:49 +0300

    First commit

---
+++ js/script.js
@@ -0,0 +1,18 @@
+"use strict";
+
+const category = 'toys';
+
+console.log(`https:someurl.com/${category}/5`);
+
+const user = 'Ivan';
+
+alert(`Привет, ${user}!`);
+
+let incr = 10,
+    decr = 10;
+
+incr++;
+decr--;
+
+console.log(incr);
+console.log(decr);
